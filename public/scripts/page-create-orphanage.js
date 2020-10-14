f2ca50dca87682a24440eff9af5a8b57b75d8422
Mateus Santos <m.santos.azeve@gmail.com>
2020-10-14 16:47:57 -0300

    regra para adicionar fotos no cadastro

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -29,3 +29,29 @@ map.on('click', event => {
   marker = L.marker([lat, lng], { icon })
     .addTo(map)
 })
+
+
+// adicionar o campo de fotos
+function addPhotoField() {
+  // pegar o container de fotos #images
+  const container = document.querySelector('#images');
+  
+  // pegar o container para duplicar .new-image
+  const fieldsContainer = document.querySelectorAll('.new-upload');
+  
+  // realizar o clone da última imagem adicionada
+  const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+  
+  // verificar se o  campo está vazio, se sim, não adicionar ao container de imagens
+  const input = newFieldContainer.children[0];
+
+  if(input.value === "") {
+    return
+  }
+  
+  // limpar o campo antes de adicionar ao container de imagens
+  input.value = '';
+  
+  // adicionar o clone ao container de #images
+  container.appendChild(newFieldContainer);
+}
