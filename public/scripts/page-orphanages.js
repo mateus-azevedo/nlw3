b2a4364306388ad4502c315d3234e3797fbaee55
Mateus Santos <m.santos.azeve@gmail.com>
2020-10-13 21:41:52 -0300

    config iniciais personalizadas do mapa

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,1 +1,27 @@
-const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+//create map
+const map = L.map('mapid').setView([-22.9025661,-43.2876696], 15);
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+//create icon
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+})
+
+//create popup overlay
+const popup = L.popup({
+  closeButton: false,
+  className: 'map-popup',
+  minWidth: 240,
+  minHeight: 240
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanages"> <img src="./public/images/arrow-white.svg" > </a>')
+
+//create and add marker
+L
+.marker([-22.9025661,-43.2876696], { icon })
+.addTo(map)
+.bindPopup(popup)
