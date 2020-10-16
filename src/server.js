e942b293413e0cca5240780630644a4a2b1bc5a7
Mateus Santos <m.santos.azeve@gmail.com>
2020-10-16 16:42:26 -0300

    iniciando meu servidor

--- src/server.js
+++ src/server.js
@@ -1,1 +1,14 @@
-console.log('Olá, tudo bem?');
+// importar dependencia 
+const express = require('express');
+const path = require('path');
+
+//iniciando o express
+const server = express()
+
+// criar uma rota
+server.get('/', (request, response) => {
+  return response.sendFile(path.join(__dirname, 'views', 'index.html'));
+})
+
+//ligar o servidor
+server.listen(5500)
