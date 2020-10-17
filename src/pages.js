fd2d29840a2142063247378011fa9021cff2e04d
Mateus Santos <m.santos.azeve@gmail.com>
2020-10-17 11:17:39 -0300

    função para salvar orfanato

--- src/pages.js
+++ src/pages.js
@@ -47,8 +47,36 @@ module.exports = {
     return res.render('create-orphanage')
   },
 
-  saveOrphanage(req, res) {
-    console.log(req.body)
+  async saveOrphanage(req, res) {
+    const fields = req.body
+
+    // validar se todos os campos estiverem preenchidos
+    if (Object.values(fields).includes('')) {
+      return res.send('Todos os campos devem ser preenchidos!')
+    }
+
+    try {
+      //salvar um orfanato
+      const db = await Database
+      await saveOrphanage(db, {
+        lat: fields.lat,
+        lng: fields.lng,
+        name: fields.name,
+        about: fields.about,
+        whatsapp: fields.whatsapp,
+        images: fields.images.toString(),
+        instructions: fields.instructions,
+        opening_hours: fields.opening_hours,
+        open_on_weekends: fields.open_on_weekends
+      })
+
+      // redirecionamento 
+      return res.redirect('/orphanages')
+    } catch (error) {
+      console.log(error)
+      return res.send('Erro banco de dados')
+    }
+    
   }
   
 }
